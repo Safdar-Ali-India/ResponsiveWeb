a4cd3c7935c9688d82862c02ea8ba2b213eee410
GHULAM SHUBHANI <ghulam@payfinix.in>
2023-04-26 23:35:10 +0530

    signup

--- src/Page/Singup.js
+++ src/Page/Singup.js
@@ -1,32 +1,47 @@
 import React from 'react'
-import {Container,Box, Grid, Typography, Button, Stack} from '@mui/material';
+import { Container, Box, Grid, Typography, Button, Stack } from '@mui/material';
 import FacebookIcon from '@mui/icons-material/Facebook';
 
 const Singup = () => {
-  return (
-    <Container sx={{backgroundColor:'#ff0000',height:'100vh',display:'flex',flexDirection:'column',
-    justifyContent:'center',alignItems:'center' }}>
-        <Box xs={12} sm={6} md={4} sx={{display:'flex',flexDirection:'column',
-        justifyContent:'center',alignItems:'center' ,backgroundColor:'#00ff00'}}>
-            <Stack  xs={12} sm={6} md={4}>
-                <Typography variant='h5'>
-                    text here
-                </Typography>
-            <Button variant="contained" endIcon={<FacebookIcon />} size="large">
-        Send
-      </Button>
-            <Button variant="contained" endIcon={<FacebookIcon />} size="large">
-        Send
-      </Button>
-            <Button variant="contained" endIcon={<FacebookIcon />} size="large">
-        Send
-      </Button>
-              
-            </Stack>
-        </Box>
+    return (
+        <Container sx={{
+            height: '100vh',
+            display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+        }}>
 
-    </Container>
-  )
+            <Box xs={12} sm={6} md={4} sx={{   }}>
+                <Stack spacing={2} sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                    <Typography variant='h5'>
+                        Welcome! How do you to get started?
+                    </Typography>
+                    <Button variant="contained" sx={{ width: '300px', }} startIcon={<FacebookIcon />} size="large">
+                        Continue with Google
+                    </Button>
+                    <Button variant="contained" sx={{ width: '300px', }} startIcon={<FacebookIcon />} size="large">
+                        Continue with Facebook
+                    </Button>
+                    <Button variant="contained" sx={{ width: '300px', }} startIcon={<FacebookIcon />} size="large">
+                        Continue with Apple
+                    </Button>
+                    <Typography variant='subtitle1'>
+                        ----------------or--------------
+                    </Typography>
+                    <Button variant="contained" sx={{ width: '350px', }} startIcon={<FacebookIcon />} size="large">
+                        Mobile Number
+                    </Button>
+
+                </Stack>
+
+            </Box>
+
+        </Container>
+    )
 }
 
 export default Singup
