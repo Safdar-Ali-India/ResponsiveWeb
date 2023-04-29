b1a4425b15db7a9758e6dcbff0ce74140f8340cf
GHULAM SHUBHANI <ghulam@payfinix.in>
2023-04-29 17:47:44 +0530

    teams

--- src/Component/PageComponent/Teams/Teamspc.jsx
+++ src/Component/PageComponent/Teams/Teamspc.jsx
@@ -1,16 +1,40 @@
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Stack, Typography, Grid,Button,Card,CardActions,CardContent,CardMedia, Container } from '@mui/material'
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 const Teamspc = () => {
-  return (
-    <Box >
-        <Stack >
-            <Typography variant='subtitle2'>
+    const navigate = useNavigate();
+    const gobackhome = ()=>{
+        navigate("/");
+    }
+    return (
+        <Box >
+            <Container>
 
-                
-            </Typography>
-        </Stack>
-    </Box>
+            <Card>
+                <Grid container>
+                    <Grid item xs={12} sm={6} md={5} sx={{ backgroundColor: '#00ff0f' }}>
+
+                        
+
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={7} sx={{ display: { xs: 'none', md: 'block' }, backgroundColor: '#00ffff' }}>
+                        hguyhio
+                    </Grid>
+                </Grid>
+            </Card>
+            <Stack  >
+                <Typography variant='subtitle2'
+                 sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}
+                 onClick={gobackhome}
+                 >
+                    <ArrowBackIosIcon />
+                    Back to homepage
+                </Typography>
+            </Stack>
+            </Container>
+        </Box>
     )
 }
 
